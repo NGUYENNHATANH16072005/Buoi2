ddcb70d23fde468154daf85f14a63cdae2004cf9
NGUYENNHATANH16072005 <nguyennhatanh16072005@gmail.com>
2024-08-25 12:36:33 +0700

    Add files via upload

---
+++ ptbac1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 13:08:52 2024
+
+@author: nhat anh
+"""
+a = float(input("Nhap he so a:"))
+b = float(input("Nhap he so b:"))
+if a == 0 :
+   print(-b/a)
+elif b == 0:
+    print("Phuong trinh Vo so nghiem")
+else:
+   print("Phuong trinh Vo nghiem")
+       
+   
